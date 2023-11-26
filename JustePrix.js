538d9edb271d1ba4392aa11a2380da0bf7b23bff
Yams Mdj <yamine.mdj@gmail.com>
2023-11-26 22:24:52 +0100

    Succesfull

--- JustePrix.js
+++ JustePrix.js
@@ -17,25 +17,27 @@
 /////////////////////////////////////////////////////////Prix Aleatoire/////////////////////////////////////////////////////////////
 
 
-number= Math.ceil(Math.random()*100) +1
+number= Math.round(Math.random()*100) +1
 let user = Number(prompt("Devine le nombre : "))
 essaie = 0
 
 for (let i = 0; i < 5; i++) {
     while (user != number) {
+        if (essaie == 5) {
+            alert('perdu')
+            console.log("perdu ! tu as atteint t'es " + essaie + "essaie")
+            break
+            }
         if(user > number)  {
             user=alert("trop grand")
         }else {
          user= alert("trop petit")
         }
-        user = prompt ("recommence")
         essaie++
-        console.log(essaie)
-        if (essaie = 5) {
-            console.log(essaie + " perdu")
-            break}}
+        console.log(essaie, number)
+        user = prompt ("recommence")
+        }
 }
     if (user == number) {
-        alert("gg tu as reussi a trouver le chiffre en " + essaie + "essaie")
+        alert("gg tu as reussi a trouver le chiffre en " + essaie + " essaie")
     }
-   
